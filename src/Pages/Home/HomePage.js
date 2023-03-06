412c7a96a4360a8741be90935c9b1fd6504c61ac
Trần Quang Huấn <hyugalaputa@gmail.com>
2023-03-06 13:39:13 +0700

    change hosting

--- src/Pages/Home/HomePage.js
+++ src/Pages/Home/HomePage.js
@@ -3,6 +3,7 @@ import SubBanner from '../../components/SubBanner/SubBanner';
 import Category from '../../components/BannerAds/Category';
 import NewList from '../../components/NewList/NewList';
 export default function HomePage() {
+    console.log(process.env.REACT_APP_BASE_URL);
     return (
         <div className="background">
             <div className="max-w-[960px] mx-auto relative bg-white">
